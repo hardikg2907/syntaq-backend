7381c96d6b19d5a7f8ab25817127cf64e7fd4108
Hardik Garg <hardikg2907@gmail.com>
2024-08-29 14:19:33 +0530

    cors allowed origins updated

--- syntaq_backend/settings.py
+++ syntaq_backend/settings.py
@@ -97,7 +97,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = "syntaq_backend.urls"
 
 CORS_URLS_REGEX = r"^/api/.*"
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://syntaq.vercel.app",
+]
 
 TEMPLATES = [
     {
